68e66c5376a64d344fcd4c1f53157a84ce8a402f
Adam Derylo <a.m.derylo@gmail.com>
2023-03-27 00:02:07 +0200

    Draft of sparse

--- DensePoly.hs
+++ DensePoly.hs
@@ -1,4 +1,7 @@
-module DensePoly() where
+{-# LANGUAGE InstanceSigs #-}
+
+module DensePoly () where
+
 import PolyClass
 import Representation
 
@@ -9,48 +12,48 @@ instance Polynomial DensePoly where
   zeroP = P []
   constP x = P [x]
   varP = P [0, 1]
-  evalP (P coeffs) x = sum $ zipWith (\c i -> c * x^i) coeffs [0..]
+  evalP (P coeffs) x = sum $ zipWith (\c i -> c * x ^ i) coeffs [0 ..]
   shiftP n (P coeffs) = P $ replicate n 0 ++ coeffs
   degree (P []) = -1
   degree (P coeffs) = length coeffs - 1
 
-
 instance (Eq a, Num a) => Num (DensePoly a) where
+  (+) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
   (P coeffs1) + (P coeffs2) = normalize $ P $ addCoeffs coeffs1 coeffs2
     where
       addCoeffs [] ys = ys
       addCoeffs xs [] = xs
-      addCoeffs (x:xs) (y:ys) = (x+y) : addCoeffs xs ys
+      addCoeffs (x : xs) (y : ys) = (x + y) : addCoeffs xs ys
 
+  negate :: (Eq a, Num a) => DensePoly a -> DensePoly a
   negate (P coeffs) = P $ map negate coeffs
 
+  (*) :: (Eq a, Num a) => DensePoly a -> DensePoly a -> DensePoly a
   (P coeffs1) * (P coeffs2) = normalize $ P $ multCoeffs coeffs1 coeffs2
     where
       multCoeffs [] _ = []
       multCoeffs _ [] = []
-      multCoeffs (x:xs) ys = addCoeffs (map (x*) ys) (0 : multCoeffs xs ys)
+      multCoeffs (x : xs) ys = addCoeffs (map (x *) ys) (0 : multCoeffs xs ys)
 
       addCoeffs [] ys = ys
       addCoeffs xs [] = xs
-      addCoeffs (x:xs) (y:ys) = (x+y) : addCoeffs xs ys
+      addCoeffs (x : xs) (y : ys) = (x + y) : addCoeffs xs ys
 
   fromInteger 0 = P []
   fromInteger n = P [fromInteger n]
 
   abs (P coeffs) = P $ map abs coeffs
 
-
 normalize (P []) = P []
 normalize (P coeffs) = P $ reverse $ dropWhile (== 0) $ reverse coeffs
 
 -- |
--- >>> x^3 - 1 :: DensePoly Integer 
+-- >>> x^3 - 1 :: DensePoly Integer
 -- P {unP = [-1,0,0,1]}
 
 -- | Num operations give canonical results:
 -- >>> isCanonicalDP (sampleDP - sampleDP)
 -- True
-    
 instance (Eq a, Num a) => Eq (DensePoly a) where
   (P xs) == (P ys) = xs == ys
 
---
+++ SparsePoly.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE InstanceSigs #-}
+module SparsePoly(fromDP, toDP, qrP) where
+
+import PolyClass
+import Representation
+
+import Data.List(sortBy, sortOn)
+import Data.Function(on)
+import qualified Data.Ord
+
+fromDP :: (Eq a, Num a) => DensePoly a -> SparsePoly a
+fromDP (P []) = S []
+fromDP (P coeffs) = S $ filter ((/= 0) . snd) $ zip [0..] coeffs
+
+toDP :: (Eq a, Num a) => SparsePoly a -> DensePoly a
+toDP (S []) = P []
+toDP (S xs) = P $ map snd xs ++ replicate (fst (last xs) - length xs + 1) 0
+
+first :: (a -> a') -> (a, b) -> (a', b)
+first f (a, b) = (f a, b)
+
+second :: (b -> b') -> (a, b) -> (a, b')
+second f (a, b) = (a, f b)
+
+instance Functor SparsePoly where
+  fmap :: (a -> b) -> SparsePoly a -> SparsePoly b
+  fmap f (S xs) = S $ map (second f) xs
+
+instance Polynomial SparsePoly where
+  zeroP :: SparsePoly a
+  zeroP = S []
+
+  constP :: (Eq a, Num a) => a -> SparsePoly a
+  constP x = S $ normalize [(0, x)]
+
+  varP :: Num a => SparsePoly a
+  varP = S [(1, 1)]
+
+  evalP :: Num a => SparsePoly a -> a -> a
+  evalP (S []) _ = 0
+  evalP (S xs) y = sum [c * y ^ i | (i, c) <- xs]
+
+  shiftP :: (Eq a, Num a) => Int -> SparsePoly a -> SparsePoly a
+  shiftP n (S xs) = S [(i + n, c) | (i, c) <- xs]
+
+  degree :: (Eq a, Num a) => SparsePoly a -> Int
+  degree (S []) = -1
+  degree (S xs) = fst $ last xs
+
+instance (Eq a, Num a) => Num (SparsePoly a) where
+  (+), (-), (*) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> SparsePoly a
+  (+) (S xs) (S ys) = S $ sortPairsByFirstDesc $ cleanUp $ combine (+) xs ys
+  (-) (S xs) (S ys) = S $ sortPairsByFirstDesc $ cleanUp $ combine (-) xs ys
+  (*) (S xs) (S ys) = S [(i+j, c*d) | (i, c) <- xs, (j, d) <- ys]
+
+  negate :: (Eq a, Num a) => SparsePoly a -> SparsePoly a
+  negate (S xs) = S $ map (second negate) xs
+
+  fromInteger :: (Eq a, Num a) => Integer -> SparsePoly a
+  fromInteger x = constP $ fromInteger x
+
+sortPairsByFirstDesc :: Ord a => [(a, b)] -> [(a, b)]
+sortPairsByFirstDesc = sortBy (\(a, _) (b, _) -> compare b a)
+
+cleanUp :: (Eq b, Num b) => [(a, b)] -> [(a, b)]
+cleanUp = filter (\(_, y) -> y /= 0)
+
+combine :: Eq a => (b -> b -> b) -> [(a, b)] -> [(a, b)] -> [(a, b)]
+combine f xs ys =
+    let common = [(x, f a b) | (x, a) <- xs, (y, b) <- ys, x == y]
+        unique = filter (\(x, _) -> x `notElem` map fst ys) xs ++ filter (\(x, _) -> notElem x (map fst xs)) ys
+        in  unique ++ common
+
+normalize :: (Eq a, Num a) => [(Int, a)] -> [(Int, a)]
+normalize xs = filter (\(_, c) -> c /= 0) $ sortOn (Data.Ord.Down . fst) xs
+
+
+instance (Eq a, Num a) => Eq (SparsePoly a) where
+    (==) :: (Eq a, Num a) => SparsePoly a -> SparsePoly a -> Bool
+    p == q = nullP(p-q)
+
+-- qrP s t | not(nullP t) = (q, r) iff s == q*t + r && degree r < degree t
+qrP :: (Eq a, Fractional a) => SparsePoly a -> SparsePoly a -> (SparsePoly a, SparsePoly a)
+qrP = undefined
+
+-- | Division example
+-- >>> qrP (x^2 - 1) (x -1) == ((x + 1), 0)
+-- True
